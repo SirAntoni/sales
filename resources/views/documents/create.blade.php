5fe78c38ff032d2e89ce97f045038aaab216c64a
Antony Culqui <a.culqui02@gmail.com>
2025-05-14 22:30:45 -0500

    fix documents

--- resources/views/documents/create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('../themes/echo')
-
-@section('subhead')
-    <title>ShiperSales | Documentos | Sistema de ventas</title>
-@endsection
-
-@section('subcontent')
-    <livewire:documents.new-document />
-@endsection
