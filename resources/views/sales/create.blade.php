915ab8814cb1b1c9fc5c04ae67e1ecec891a0851
Antony Culqui <a.culqui02@gmail.com>
2025-05-14 22:16:54 -0500

    fix create sale

---
+++ resources/views/sales/create.blade.php
@@ -0,0 +1,9 @@
+@extends('../themes/echo')
+
+@section('subhead')
+    <title>ShiperSales | Ventas | Sistema de ventas</title>
+@endsection
+
+@section('subcontent')
+    <livewire:sales.new-sale />
+@endsection
