33775ffdc51a11a03a1b5a0cac6a03d2e7263823
Antony Culqui <a.culqui02@gmail.com>
2025-05-29 00:49:01 -0500

    testing migrations

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,15 @@ Route::middleware(['auth'])->group(function () {
 //    });
     //End create password
 
+    Route::get('/run-migrations', function () {
+        // Ejecuta todas las migraciones pendientes con --force
+        Artisan::call('migrate', ['--force' => true]);
+
+        // Devuelve por pantalla el resultado de Artisan
+        return response( nl2br(Artisan::output()), 200 )
+            ->header('Content-Type', 'text/html');
+    });
+
 });
 
 
